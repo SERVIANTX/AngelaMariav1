3ea8cf93e36adb50d4890da2bd943692ff6edbed
yurimendozavilcahuaman <yurimendozavilcahuaman@gmail.com>
2022-02-11 17:24:57 -0500

    MODELO DE TABLA EMPLEADO - UUID

--- laravel/app/Models/Empleado.php
+++ laravel/app/Models/Empleado.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use BinaryCabin\LaravelUUID\Traits\HasUUID;
 
 class Empleado extends Model
 {
-    use HasFactory;
+
+    use HasUUID;
+    protected $table = 'Empleado';
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $uuidFieldName = 'id';
 }
